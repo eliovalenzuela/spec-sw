1d8706eef80c0c9bc2a4585b526b45b29c337040
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2012-05-22 18:00:59 +0200

    tools: added spec-vuart, spec-fwloader, multiple SPEC support & base address settings

--- tools/spec-tools.h
+++ tools/spec-tools.h
@@ -1,9 +1,12 @@
 
-uint32_t spec_get_base(int basenr);
+#include "loader-userspace.h"
+
+void *spec_map_area(int bus, int dev, int bar, size_t size);
 
 enum {
 	BASE_BAR0 = 0,	/* for wrpc etc (but lm32 is at 0x80000 offset) */
-	BASE_BAR2,
-	BASE_BAR4	/* for gennum-internal registers */
-};
+	BASE_BAR2 = 2,
+	BASE_BAR4 = 4	/* for gennum-internal registers */
+};           
+
 
