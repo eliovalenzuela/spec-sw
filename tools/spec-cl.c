a7e4be1e751e9e965f7dc9bc811d768def443360
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2012-05-31 17:07:35 +0200

    spec-tools: moved the core functionality into a small library

--- tools/spec-cl.c
+++ tools/spec-cl.c
@@ -7,31 +7,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <getopt.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-#include "spec-tools.h"
+#include "speclib.h"
 
 int main(int argc, char **argv)
 {
-	int bus = -1, dev_fn = -1, i, c;
-	struct stat stbuf;
-	unsigned char *buf;
-	unsigned int *ibuf;
-	volatile uint32_t *p;
-	void *map_base;
-	char *fname;
+	int bus = -1, dev_fn = -1, c;
 	uint32_t lm32_base = 0x80000;
-	
-	FILE *f;
-	
+	void *card;
+
 
 	while ((c = getopt (argc, argv, "b:d:c:")) != -1)
 	{
@@ -48,9 +33,9 @@ int main(int argc, char **argv)
 			break;
 		default:
 			fprintf(stderr,
-				"Use: \"%s [-b bus] [-d devfn] <fpga_bitstream.bin>\"\n", argv[0]);
+				"Use: \"%s [-b bus] [-d devfn] [-c lm32 base address] <lm32_program.bin>\"\n", argv[0]);
 			fprintf(stderr,
-				"By default, the first available SPEC is used and the LM32 is assumet at 0x%x.\n", lm32_base);
+				"By default, the first available SPEC is used and the LM32 is assumed at 0x%x.\n", lm32_base);
 			exit(1);
 		}
 	}
@@ -60,76 +45,20 @@ int main(int argc, char **argv)
 		exit(1);
 	}
     
-	fname = argv[optind];
-
-	f = fopen(fname, "r");
-	if (!f) {
-		fprintf(stderr, "%s: %s: %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (fstat(fileno(f), &stbuf)) {
-		fprintf(stderr, "%s: stat(%s): %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (!S_ISREG(stbuf.st_mode)) {
-		fprintf(stderr, "%s: %s: Not a regular file\n", argv[0],
-			fname);
-		exit(1);
-	}
-
-	buf = malloc(stbuf.st_size);
-	if (!buf) {
-		fprintf(stderr, "%s: Can't allocate buffer (%li bytes): %s\n",
-			argv[0], (long)stbuf.st_size, strerror(errno));
-		exit(1);
-	}
-	i = fread(buf, 1, stbuf.st_size, f);
-	if (i < 0) {
-		fprintf(stderr, "%s: read(%s): %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (i != stbuf.st_size) {
-		fprintf(stderr, "%s: short read from %s\n", argv[0], fname);
-		exit(1);
-	}
-
-	map_base = spec_map_area(bus, dev_fn, BASE_BAR0, 0x100000);
-	if(!map_base)
+    card = spec_open(bus, dev_fn);
+	if(!card)
 	{
-		fprintf(stderr, "%s: can't map the SPEC @ %02x:%02x\n", argv[0], bus, dev_fn);
-		exit(1);
+	 	fprintf(stderr, "Can't detect a SPEC card under the given adress. Make sure a SPEC card is present in your PC and the driver is loaded.\n");
+	 	exit(1);
 	}
 
-	ibuf = (void *)buf;
-
-	/* Phew... we are there, finally */
-	*(volatile uint32_t *)(map_base + lm32_base + 0x20400) = 0x1deadbee;
-	while ( !((*(volatile uint32_t *)(map_base + lm32_base + 0x20400)) & (1<<28)) )
-		;
-
-	p = map_base + lm32_base;
-	for (i = 0; i < (stbuf.st_size + 3) / 4; i++) {
-		p[i] = htonl(ibuf[i]); /* big endian */
+	if(spec_load_lm32(card, argv[optind], lm32_base) < 0)
+	{
+	 	fprintf(stderr, "Loader failure.\n");
+	 	exit(1);
 	}
 
-	sync();
-
-	for (i = 0; i < (stbuf.st_size + 3) / 4; i++) {
-		if (p[i] != htonl(ibuf[i]))
-			fprintf(stderr, "programming error at %x "
-				"(expected %08x, found %08x)\n", i*4,
-				htonl(ibuf[i]), p[i]);
-	}
-
-	sync();
-
-	*(volatile uint32_t *)(map_base + lm32_base + 0x20400) = 0x0deadbee;
-
-	if (getenv("VERBOSE"))
-		printf("%s: Wrote %li bytes at offset 0x%x\n", argv[0],
-		       (long)stbuf.st_size, lm32_base);
+	spec_close(card);
+	
 	exit (0);
 }
--- tools/spec-fwloader.c
+++ tools/spec-fwloader.c
@@ -1,5 +1,5 @@
 /*
- * A tool to program our soft-core (LM32) within the SPEC.
+ * A tool to program the FPGA within the SPEC.
  *
  * Alessandro Rubini 2012 for CERN, GPLv2 or later.
  */
@@ -7,28 +7,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <getopt.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-#include "spec-tools.h"
+#include "speclib.h"
 
 int main(int argc, char **argv)
 {
-	int bus = -1, dev_fn = -1, i, c;
-	struct stat stbuf;
-	unsigned char *buf;
-	void *map_base;
-	char *fname;
-	
-	FILE *f;
-	
+	int bus = -1, dev_fn = -1, c;
+	void *card;
 
 	while ((c = getopt (argc, argv, "b:d:")) != -1)
 	{
@@ -54,50 +40,20 @@ int main(int argc, char **argv)
 		exit(1);
 	}
     
-	fname = argv[optind];
-
-	f = fopen(fname, "r");
-	if (!f) {
-		fprintf(stderr, "%s: %s: %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (fstat(fileno(f), &stbuf)) {
-		fprintf(stderr, "%s: stat(%s): %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (!S_ISREG(stbuf.st_mode)) {
-		fprintf(stderr, "%s: %s: Not a regular file\n", argv[0],
-			fname);
-		exit(1);
-	}
-
-	buf = malloc(stbuf.st_size);
-	if (!buf) {
-		fprintf(stderr, "%s: Can't allocate buffer (%li bytes): %s\n",
-			argv[0], (long)stbuf.st_size, strerror(errno));
-		exit(1);
-	}
-	i = fread(buf, 1, stbuf.st_size, f);
-	if (i < 0) {
-		fprintf(stderr, "%s: read(%s): %s\n", argv[0], fname,
-			strerror(errno));
-		exit(1);
-	}
-	if (i != stbuf.st_size) {
-		fprintf(stderr, "%s: short read from %s\n", argv[0], fname);
-		exit(1);
-	}
-
-	map_base = spec_map_area(bus, dev_fn, BASE_BAR4, 0x1000);
-	if(!map_base)
+    card = spec_open(bus, dev_fn);
+	if(!card)
 	{
-		fprintf(stderr, "%s: can't map the SPEC @ %02x:%02x\n", argv[0], bus, dev_fn);
-		exit(1);
+	 	fprintf(stderr, "Can't detect a SPEC card under the given adress. Make sure a SPEC card is present in your PC and the driver is loaded.\n");
+	 	exit(1);
 	}
 
-	loader_low_level(0, map_base, buf, stbuf.st_size);
+	if(spec_load_bitstream(card, argv[optind]) < 0)
+	{
+	 	fprintf(stderr, "Loader failure.\n");
+	 	exit(1);
+	}
 
+	spec_close(card);
+	
 	exit (0);
 }
