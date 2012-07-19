239c63bae15f7bf08b68346757a0a1515f26e576
Alessandro Rubini <rubini@gnudd.com>
2012-07-19 13:01:49 +0200

    spec.h: more gennum registers and minor cleanups

--- kernel/spec.h
+++ kernel/spec.h
@@ -10,10 +10,8 @@
 #ifndef __SPEC_H__
 #define __SPEC_H__
 #include <linux/pci.h>
-#include <linux/workqueue.h>
 #include <linux/firmware.h>
-#include <linux/atomic.h>
-#include <linux/list.h>
+#include <linux/completion.h>
 #include <linux/fmc.h>
 
 #define PCI_VENDOR_ID_CERN	0x10dc
@@ -27,43 +25,87 @@
 struct spec_dev {
 	struct pci_dev		*pdev;
 	struct resource		*area[3];	/* bar 0, 2, 4 */
-	void			*remap[3];	/* ioremap of bar 0, 2, 4 */
+	void __iomem		*remap[3];	/* ioremap of bar 0, 2, 4 */
 	char			*submod_name;
 	struct work_struct	work;
 	const struct firmware	*fw;
 	struct list_head	list;
 	unsigned long		irqcount;
-	atomic_t		has_submod;
 	void			*sub_priv;
 	struct fmc_device	*fmc;
+	int			irq_count;	/* for mezzanine use too */
+	struct completion	compl;
 };
 
-/* Registers from the gennum header files */
+/* Registers for GN4124 access */
 enum {
-	GNGPIO_BASE = 0xA00,
-	GNGPIO_DIRECTION_MODE = GNGPIO_BASE + 0x4,
-	GNGPIO_OUTPUT_ENABLE = GNGPIO_BASE + 0x8,
-	GNGPIO_OUTPUT_VALUE = GNGPIO_BASE + 0xC,
-	GNGPIO_INPUT_VALUE = GNGPIO_BASE + 0x10,
+	/* page 106 */
+	GNPPCI_MSI_CONTROL	= 0x48,		/* actually, 3 smaller regs */
+	GNPPCI_MSI_ADDRESS_LOW	= 0x4c,
+	GNPPCI_MSI_ADDRESS_HIGH	= 0x50,
+	GNPPCI_MSI_DATA		= 0x54,
 
-	FCL_BASE	= 0xB00,
-	FCL_CTRL	= FCL_BASE,
-	FCL_STATUS	= FCL_BASE + 0x4,
-	FCL_IODATA_IN	= FCL_BASE + 0x8,
-	FCL_IODATA_OUT	= FCL_BASE + 0xC,
-	FCL_EN		= FCL_BASE + 0x10,
-	FCL_TIMER_0	= FCL_BASE + 0x14,
-	FCL_TIMER_1	= FCL_BASE + 0x18,
-	FCL_CLK_DIV	= FCL_BASE + 0x1C,
-	FCL_IRQ		= FCL_BASE + 0x20,
-	FCL_TIMER_CTRL	= FCL_BASE + 0x24,
-	FCL_IM		= FCL_BASE + 0x28,
-	FCL_TIMER2_0	= FCL_BASE + 0x2C,
-	FCL_TIMER2_1	= FCL_BASE + 0x30,
-	FCL_DBG_STS	= FCL_BASE + 0x34,
-	FCL_FIFO	= 0xE00,
-	PCI_SYS_CFG_SYSTEM = 0x800
+	/* page 130 ff */
+	GNINT_CTRL		= 0x810,
+	GNINT_STAT		= 0x814,
+	GNINT_CFG_0		= 0x820,
+	GNINT_CFG_1		= 0x824,
+	GNINT_CFG_2		= 0x828,
+	GNINT_CFG_3		= 0x82c,
+	GNINT_CFG_4		= 0x830,
+	GNINT_CFG_5		= 0x834,
+	GNINT_CFG_6		= 0x838,
+	GNINT_CFG_7		= 0x83c,
+#define GNINT_CFG(x) (GNINT_CFG_0 + 4 * (x))
+
+	/* page 146 ff */
+	GNGPIO_BASE = 0xA00,
+	GNGPIO_BYPASS_MODE	= GNGPIO_BASE,
+	GNGPIO_DIRECTION_MODE	= GNGPIO_BASE + 0x04, /* 0 == output */
+	GNGPIO_OUTPUT_ENABLE	= GNGPIO_BASE + 0x08,
+	GNGPIO_OUTPUT_VALUE	= GNGPIO_BASE + 0x0C,
+	GNGPIO_INPUT_VALUE	= GNGPIO_BASE + 0x10,
+	GNGPIO_INT_MASK		= GNGPIO_BASE + 0x14, /* 1 == disabled */
+	GNGPIO_INT_MASK_CLR	= GNGPIO_BASE + 0x18, /* irq enable */
+	GNGPIO_INT_MASK_SET	= GNGPIO_BASE + 0x1C, /* irq disable */
+	GNGPIO_INT_STATUS	= GNGPIO_BASE + 0x20,
+	GNGPIO_INT_TYPE		= GNGPIO_BASE + 0x24, /* 1 == level */
+	GNGPIO_INT_VALUE	= GNGPIO_BASE + 0x28, /* 1 == high/rise */
+	GNGPIO_INT_ON_ANY	= GNGPIO_BASE + 0x2C, /* both edges */
+
+	/* page 158 ff */
+	FCL_BASE		= 0xB00,
+	FCL_CTRL		= FCL_BASE,
+	FCL_STATUS		= FCL_BASE + 0x04,
+	FCL_IODATA_IN		= FCL_BASE + 0x08,
+	FCL_IODATA_OUT		= FCL_BASE + 0x0C,
+	FCL_EN			= FCL_BASE + 0x10,
+	FCL_TIMER_0		= FCL_BASE + 0x14,
+	FCL_TIMER_1		= FCL_BASE + 0x18,
+	FCL_CLK_DIV		= FCL_BASE + 0x1C,
+	FCL_IRQ			= FCL_BASE + 0x20,
+	FCL_TIMER_CTRL		= FCL_BASE + 0x24,
+	FCL_IM			= FCL_BASE + 0x28,
+	FCL_TIMER2_0		= FCL_BASE + 0x2C,
+	FCL_TIMER2_1		= FCL_BASE + 0x30,
+	FCL_DBG_STS		= FCL_BASE + 0x34,
+
+	FCL_FIFO		= 0xE00,
+
+	PCI_SYS_CFG_SYSTEM	= 0x800
 };
+
+/* Access gennum registers in a "standard" way */
+static inline uint32_t gennum_readl(struct spec_dev *spec, int reg)
+{
+	return readl(spec->remap[2] + reg);
+}
+static inline void gennum_writel(struct spec_dev *spec, uint32_t val, int reg)
+{
+	writel(val, spec->remap[2] + reg);
+}
+
+
 
 /* Functions in spec-fmc.c, used by spec-pci.c */
 extern int spec_fmc_create(struct spec_dev *spec);
