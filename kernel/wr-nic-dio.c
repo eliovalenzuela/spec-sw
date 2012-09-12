5993649abf9f6fc562ec28df0e14996e60482d59
Alessandro Rubini <rubini@gnudd.com>
2012-09-12 19:44:50 +0200

    kernel: add wr-nic-dio with mezzanine ioctl, currently empty
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

---
+++ kernel/wr-nic-dio.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2012 CERN (www.cern.ch)
+ * Author: Alessandro Rubini <rubini@gnudd.com>
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ *
+ * This work is part of the White Rabbit project, a research effort led
+ * by CERN, the European Institute for Nuclear Research.
+ */
+#include <linux/module.h>
+#include <linux/fmc.h>
+#include <linux/fmc-sdb.h>
+#include "spec.h"
+#include "spec-nic.h"
+#include "wr_nic/wr-nic.h"
+
+int wrn_mezzanine_ioctl(struct net_device *dev, struct ifreq *rq,
+			       int cmd)
+{
+	return -EAGAIN;
+}
